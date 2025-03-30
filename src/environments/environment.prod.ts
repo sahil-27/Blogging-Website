c22eb177762cefd3162d68abaa4a5e605d0db092
sahil_01 <sahil@smartinterviews.in>
2025-03-30 18:39:10 +0530

    minor change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
 	production: true,
-	apiUrl: 'http://nodejsproject-env.eba-bmu8wbiw.ap-south-1.elasticbeanstalk.com/api'
+	apiUrl: 'https://myblogs-backend-7hpj.onrender.com/api'
+	// apiUrl: 'http://nodejsproject-env.eba-bmu8wbiw.ap-south-1.elasticbeanstalk.com/api'
 };
